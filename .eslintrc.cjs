9f27b3bcbd67686bffe11f0790ad159e16ba7e00
MatteoCinti <matteo.cinti@barnebys.com>
2024-06-12 13:56:06 +0200

    installed linter and prettier on project levl

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks', // check
+    'plugin:jsx-a11y/recommended', // check
+    'plugin:import/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: [
+      './tsconfig.json',
+      './@app/client/tsconfig.json',
+      './@app/client/tsconfig.node.json'
+    ],
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['@typescript-eslint', 'simple-import-sort', 'import'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/named': 'off',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'react/react-in-jsx-scope': 'off'
+  }
+};
