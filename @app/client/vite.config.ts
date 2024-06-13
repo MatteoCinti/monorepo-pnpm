a51397f06f95404eca554751067eecaaa1daca2c
MatteoCinti <matteo.cinti@barnebys.com>
2024-06-13 08:24:24 +0200

    bad ts experience

--- @app/client/vite.config.ts
+++ @app/client/vite.config.ts
@@ -1,12 +1,13 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import eslint from 'vite-plugin-eslint';
+// @ts-ignore - eslintPlugin is not typed
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    eslint({
+    eslintPlugin({
       include: ['src/**/*.ts', 'src/**/*.tsx']
     })
   ]
